a22d54369b9a3c3328bd392bba2b7f58add40f42
IlTuoNome <laTuaEmail@example.com>
2025-07-23 15:01:58 +0200

    Complete snack 3

--- snack.js
+++ snack.js
@@ -9,7 +9,17 @@ function createSlug(title) {
   return title.toLowerCase();
 }
 
+// Snack 3
+function average(numbers) {
+  let sum = 0;
+  numbers.forEach((num) => {
+    sum += num;
+  });
+  return sum / numbers.length;
+}
+
 module.exports = {
   getInitials,
   createSlug,
+  average,
 };
