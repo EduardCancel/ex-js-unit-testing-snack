b6ad38b39ee378fc1a54790742e546a9b6e4760c
IlTuoNome <laTuaEmail@example.com>
2025-07-23 14:58:27 +0200

    Complete snack 2

--- snack.js
+++ snack.js
@@ -1,8 +1,15 @@
+// Snack 1
 function getInitials(name) {
   const [nome, cognome] = name.split(" ").filter((str) => str !== "");
   return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}`;
 }
 
+// Snack 2
+function createSlug(title) {
+  return title.toLowerCase();
+}
+
 module.exports = {
   getInitials,
+  createSlug,
 };
--- snack.test.js
+++ snack.test.js
@@ -1,6 +1,11 @@
-const { getInitials } = require("./snack.js");
+const { getInitials, createSlug } = require("./snack.js");
 
 // Snack 1
 test("La funzione getInitials restituisce le iniiziali di un nome completo", () => {
   expect(getInitials("John Doe")).toBe("J.D");
+});
+
+// Snack 2
+test("La funzione createSlug restituisce una stringa in lowercase,", () => {
+  expect(createSlug("Hello World")).toBe("hello world");
 });
